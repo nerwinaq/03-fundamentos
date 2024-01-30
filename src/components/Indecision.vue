597ec4114d241a6d1cde7e79eac675cc83d10bbb
nerwinaq <nelsonerwin_a@outlook.com>
2024-01-30 18:48:29 -0400

    30012024

---
+++ src/components/Indecision.vue
@@ -0,0 +1,17 @@
+<template>
+    <img src="" alt="">
+    <h1>Indecisión</h1>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
